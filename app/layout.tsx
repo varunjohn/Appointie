fa2a28b84e8f1d0080147a1f9e573a0866a4717c
vjohnConestoga <vjohn@conestogac.on.ca>
2025-04-08 00:09:24 -0400

    layout issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default async function RootLayout({
   const currentUser = await getCurrentUser();
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-slate-300`}>
         <>
           <ClientOnly>
             <ToasterProvider />
